d6ff60a864059478ff39be089162283ecd58d946
kdkaanev <kdkaanev@github.com>
2024-04-10 23:38:42 +0300

    static files

--- guitar_house/guitar_house/settings.py
+++ guitar_house/guitar_house/settings.py
@@ -137,9 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles"
     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
