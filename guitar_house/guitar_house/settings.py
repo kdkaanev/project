0ae208928f95af073fd6be0e6fbc3d42782dcd34
kdkaanev <kdkaanev@github.com>
2024-04-11 11:56:50 +0300

    Merge remote-tracking branch 'project/master'

--- guitar_house/guitar_house/settings.py
+++ guitar_house/guitar_house/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'guitar_house.wsgi.application'
 if DEBUG:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "house_db",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "127.0.0.1",
-            "PORT": "5432",
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 else:
