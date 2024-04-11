77d2ef335f798c07b2f23fd1795a0b372fa8bfd1
kdkaanev <kdkaanev@github.com>
2024-04-11 12:25:35 +0300

    Merge remote-tracking branch 'project/master'

--- guitar_house/guitar_house/settings.py
+++ guitar_house/guitar_house/settings.py
@@ -149,6 +149,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles"
     ]
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / ('mediafiles')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
