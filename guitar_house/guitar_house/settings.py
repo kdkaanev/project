247b9e102f4d9b6525cf9b3c56b7446b36d4bc60
kdkaanev <kdkaanev@github.com>
2024-04-18 21:07:43 +0300

    debug true

--- guitar_house/guitar_house/settings.py
+++ guitar_house/guitar_house/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', 'False')
+DEBUG = os.environ.get('DEBUG', '1')
 
 # ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(' ')
 
