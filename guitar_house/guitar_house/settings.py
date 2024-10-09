498257ad96aab49732bdb2b242c9227929562354
Kancho <103933652+kdkaanev@users.noreply.github.com>
2024-10-09 10:11:09 +0300

    fix database error

--- guitar_house/guitar_house/settings.py
+++ guitar_house/guitar_house/settings.py
@@ -99,20 +99,8 @@ WSGI_APPLICATION = 'guitar_house.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': 'new_base_db',
-#             'USER': 'postgres',
-#             'PASSWORD': 'postgres',
 
-
-
-#             'HOST': '127.0.0.1',
-#             'PORT': '5432',
-#         }
-#     }
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('POSTGRES_DB'),
@@ -121,7 +109,7 @@ WSGI_APPLICATION = 'guitar_house.wsgi.application'
             'HOST': os.getenv('POSTGRES_HOST'),
             'PORT': os.getenv('POSTGRES_PORT'),
         }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
